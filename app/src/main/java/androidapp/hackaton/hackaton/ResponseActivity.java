acd84d07d6d4c59807182e6cf53fe8c87bcdec5e
ielay <ielay@yandex.ru>
2018-10-27 17:36:43 +0300

    Add second one activity

---
+++ app/src/main/java/androidapp/hackaton/hackaton/ResponseActivity.java
@@ -0,0 +1,18 @@
+package androidapp.hackaton.hackaton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class ResponseActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_response);
+
+        listView = findViewById(R.layout.)
+    }
+}
